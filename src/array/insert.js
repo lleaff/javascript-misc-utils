55cc5fcdf1e6352f738a529872b3890525283b9b
lleaff <lleaff@outlook.com>
2017-04-11 08:13:55 +0100

    >flow array notation

--- src/array/insert.js
+++ src/array/insert.js
@@ -3,9 +3,9 @@
  *
  * @param index - Infinity will evaluate to array length (= append behavior).
  */
-export default function arrayInsert(arr: [any],
+export default function arrayInsert(arr: any[],
                                     index: int,
-                                    toInsert: any): [any] {
+                                    toInsert: any): any[] {
     if (index === Infinity) {
         index = arr.length;
     }
